a6672b6fc948b9aede1da2b6bb09b24e2e002855
dgrills <devan.grills@gmail.com>
2023-01-10 12:45:23 -0500

    Update sketch.js

--- CreativeCoding/Grid_GIF/sketch.js
+++ CreativeCoding/Grid_GIF/sketch.js
@@ -15,12 +15,18 @@ function draw() { //runs in a loop
 fill (0, 0, 255); //fills the quad pink
 strokeWeight (1); //stroke weight 8 pixels
 
-var num = 2; // number of squares in my array
+var num = 16; // number of squares in my array
 var sideLength = windowWidth / num; // side length
 
-for (var y = 0; y < windowWidth; y = y + sideLength){ //loop creates a row in the y direction
-  for (var x = 0; x < windowWidth; x = x + sideLength){ //loop creates a row in the x direction
+translate (-150, -150); //this will give your design a bleed effect
+
+for (var y = 0; y < 2 * windowWidth; y = y + sideLength){ //loop creates a row in the y direction
+  for (var x = 0; x < 2 * windowWidth; x = x + sideLength){ //loop creates a row in the x direction
           image (gif, x, y, windowWidth/num, windowWidth/num);
 }
 }
+}
+
+function windowResized (){ //this resizes and refreshes your design with the brower
+  resizeCanvas (windowWidth, windowHeight);
 }
